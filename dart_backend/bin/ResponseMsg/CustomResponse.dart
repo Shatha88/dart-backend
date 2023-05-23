cd3d252c55016ecb2436810bfcd47df41612faa2
Khalidsaadhabash <132256722+Khalidsaadhabash@users.noreply.github.com>
2023-05-23 18:47:43 +0300

    chang statuscode

--- dart_backend/bin/ResponseMsg/CustomResponse.dart
+++ dart_backend/bin/ResponseMsg/CustomResponse.dart
@@ -7,14 +7,14 @@ import 'package:shelf/shelf.dart';
 class CustomResponse {
   errorResponse({required String msg, required String? statusCode}) {
     return Response.badRequest(
-      body: json.encode({"msg": msg, "statusCode": statusCode}),
+      body: json.encode({"msg": msg, "statusCode": 400}),
       headers: {"content-type": "applecation/json"},
     );
   }
 
   successResponse({required String msg, required int statusCode, List? data}) {
     return Response.ok(
-      json.encode({"msg": msg, "statusCode": statusCode, 'data': data}),
+      json.encode({"msg": msg, "statusCode": 200, ...data ?? {}}),
       headers: {"content-type": "applecation/json"},
     );
   }

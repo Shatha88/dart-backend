da74c2d7add5fc00a9394dafed32a3dc2da052a7
Khalidsaadhabash <132256722+Khalidsaadhabash@users.noreply.github.com>
2023-05-23 20:07:01 +0300

    createRes1

--- dart_backend/bin/Responses/Auth/createUserResponse.dart
+++ dart_backend/bin/Responses/Auth/createUserResponse.dart
@@ -1,21 +1,38 @@
 // ignore_for_file: file_names
 
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:supabase/supabase.dart';
 
-import '../../ResponseMsg/CustomResponse.dart';
+import '../../Models/UserModel.dart';
+import '../../RespnseMsg/CustomResponse.dart';
+import '../../Services/Supabase/supabaseEnv.dart';
 
-createUserResponse(Request _) async {
+createResponse(Request req) async {
   try {
-    return CustomResponse().successResponse(
-      msg: "",
-      statusCode: 201,
-      data: [],
+    final body = json.decode(await req.readAsString());
+    print(body);
+
+    final supabaseVariable = SupabaseEnv().supabase.auth;
+    UserResponse userInfo = await supabaseVariable.admin.createUser(
+      AdminUserAttributes(email: body['email'], password: body['password'],),
     );
-  } on AuthException catch (error) {
-    return CustomResponse()
-        .errorResponse(msg: error.message, statusCode: error.statusCode);
-  } on Exception catch (error) {
-    return CustomResponse().errorResponse(msg: '$error', statusCode: '400');
+    UserModel userObject =
+        UserModel(email: userInfo.user!.email!, idAuth: userInfo.user!.id, username: '');
+
+    await supabaseVariable.signInWithOtp(email: body['email']);
+    await SupabaseEnv().supabase.from("user1").insert(userObject.toMap());
+
+    return CustomResponse().successResponse(
+      msg: "create account page",
+      data: {"email": body['email']},
+    );
+  } catch (error) {
+    print(error);
+
+    return CustomResponse().errorResponse(
+      msg: "sorry you should have body",
+    );
   }
 }

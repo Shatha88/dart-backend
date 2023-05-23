91844c42a27a04ae288b1cb4c8cefa1c0fc69cf9
shatha althbiti <107032419+Shatha88@users.noreply.github.com>
2023-05-23 18:19:26 +0300

    Add files via upload

--- dart_backend/bin/Routes/UserRoute.dart
+++ dart_backend/bin/Routes/UserRoute.dart
@@ -6,6 +6,7 @@ import 'package:shelf_router/shelf_router.dart';
 import '../Middlewares/checkTokenMiddleware.dart';
 import '../Responses/User/addContactResponse.dart';
 import '../Responses/User/deleteContactResponse.dart';
+import '../Responses/User/displayAllContact.dart';
 import '../Responses/User/displayContactResponse.dart';
 import '../Responses/User/displayUserContact.dart';
 
@@ -13,9 +14,10 @@ class UserRoute {
   Handler get handler {
     final router = Router()
       ..post('/add_contact', addContactResponse) // Add contact
-      ..get("/my_contact", displayContactResponse) // display contact
+      ..get("/view_contact/<id>", displayContactResponse) // display contact
       ..delete('/delete_mycontact/<id>', deleteContactResponse) //delete contact
-      ..get("/display_user", displayUserContact); // display user => contact
+      ..get("/display_user/<id>", displayUserContact) // display user => contact
+      ..get('/view_allcontact', displayAllContact);
 
     final pipline =
         Pipeline().addMiddleware(checkTokenMiddleware()).addHandler(router);

bf7b080f8b8adccc35df9912f3621f40d261170a
shatha althbiti <107032419+Shatha88@users.noreply.github.com>
2023-05-23 18:15:36 +0300

    Add files via upload

---
+++ dart_backend/displayAllContact.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: file_names
+
+import 'package:shelf/shelf.dart';
+import 'package:supabase/supabase.dart';
+
+import '../../ResponseMsg/CustomResponse.dart';
+
+displayAllContact(Request _) async {
+  try {
+    return CustomResponse().successResponse(
+      msg: "",
+      statusCode: 201,
+      data: [],
+    );
+  } on AuthException catch (error) {
+    return CustomResponse()
+        .errorResponse(msg: error.message, statusCode: error.statusCode);
+  } on Exception catch (error) {
+    return CustomResponse().errorResponse(msg: '$error', statusCode: '400');
+  }
+}
